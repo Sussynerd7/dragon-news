d02466a973030ba84ba51ad4ad49c8e2347777bf
sussunerd7 <sifatforpc999@gmail.com>
2025-10-18 12:23:19 +0600

    first commit

---
+++ src/Homelayout.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Outlet } from 'react-router';
+import Header from './components/Header';
+
+const Homelayout = () => {
+    return (
+ <div>
+    
+   <h1>he</h1>
+     </div>
+    );
+};
+
+export default Homelayout;
---
+++ src/routes/router.jsx
@@ -0,0 +1,13 @@
+import { createBrowserRouter } from "react-router";
+import Homelayout from "../Homelayout";
+
+
+export const router = createBrowserRouter([
+    {
+        path:"/",
+     Component:Homelayout
+    },
+   
+    
+    
+])
